cb5c45911a5d240f68a44708670d22c3706016e2
Gulia <gulia.isaeva.pls@gmail.com>
2022-12-05 22:58:45 +0300

    refactor: change path

--- pages/main/script.js
+++ pages/main/script.js
@@ -199,7 +199,7 @@ shoppingCartContent.appendChild(confirmBtn);
 //function to render carts taking book infos from json file
 
  async function renderCatalogCarts(){
-    let url = '../main/catalogBooks.json';
+    let url = 'catalogBooks.json';
     let data = await fetch(url);
     let dataFrom= await data.json();
     console.log(dataFrom)
